1f7af06f232b11e1eead54e04202232d91c30a45
Candy <candy.ongaro@student.moringaschool.com>
2023-12-15 16:27:15 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const names = ["Guadalupe","Ollie", "Aki"];
+
+function writeCards(names, wedding){
+
+    let messages = [];
+    for ( let i = 0; i < names.length; i++){
+       let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`
+       messages.push(message); 
+    }
+ return messages;
+}
+
+
+function countDown(startingNumber) {
+    for (let i = startingNumber; i >= 0; i--) {
+        console.log(i);
+    }
+}
